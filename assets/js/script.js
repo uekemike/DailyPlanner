76eb101a0b8b590881d0d10e44ad47d9924425c6
Ufuoma Ekemike <uekemike@yahoo.com>
2021-07-31 23:11:45 -0400

    create js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,27 @@
     //set the text of the p tag as the current day
     $("#currentDay").text(currentDay);
 
-    var saveItemsBtn =document.querySelector('saveBtn');
-    var textReceived =document.querySelector('.text');
+
+    var hour9 =$("#9");
+    var hour10 =$("#10");
+    var hour10 =$("#11");
+    var hour10 =$("#12");
+    var hour10 =$("#13");
+    var hour10 =$("#14");
+    var hour10 =$("#15");
+    var hour10 =$("#16");
+    var time = moment();
+
+//     var saveItemsBtn =document.querySelector('saveBtn');
+//     var textReceived =document.querySelector('.text');
     
-    textReceived.value =localStorage.getItem('content');
+//     textReceived.value =localStorage.getItem('content');
 
-   function savedPlanner(){
-       //use local storage to save get infor from planner and save
-       localStorage.setItem('content', textReceived.value);
+//    function savedPlanner(){
+//        //use local storage to save get infor from planner and save
+//        localStorage.setItem('content', textReceived.value);
 
-   };
+//    };
 
-    saveItemsBtn.addEventListener("click", savedPlanner);
+//     saveItemsBtn.addEventListener("click", savedPlanner);
 
